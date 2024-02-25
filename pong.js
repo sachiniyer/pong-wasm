acc50013439ad466431182f5116863af104861ca
Sachin Iyer <sachinjiyer@gmail.com>
2024-02-25 02:20:19 -0800

    better resizing and less jank

--- pong.js
+++ pong.js
@@ -1,145 +1,219 @@
 const canvas = document.getElementById("pongCanvas");
 const context = canvas.getContext("2d");
-
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
-
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
-const paddleWidth = canvas.width * 0.01;
-const paddleHeight = canvas.height * 0.2;
-const paddleSpeed = canvas.height * 0.02;
-const ballSize = Math.min(canvas.width, canvas.height) * 0.02;
-const ballSpeed = Math.min(canvas.width, canvas.height) * 0.005;
+const title = "Pong";
 
-let player1Score = 0;
-let player2Score = 0;
+const Direction = {
+  UP: "UP",
+  DOWN: "DOWN",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+};
 
-let player1Y = canvas.height / 2 - paddleHeight / 2;
-let player2Y = canvas.height / 2 - paddleHeight / 2;
+let paddleConfig = {
+  width: 0.01,
+  height: 0.2,
+  speed: 0.02,
+};
 
-let ballX = canvas.width / 2;
-let ballY = canvas.height / 2;
-let ballSpeedX = ballSpeed;
-let ballSpeedY = ballSpeed;
+let ballConfig = {
+  size: 0.02,
+  dx: 0.005,
+  dy: 0.005,
+};
 
-function drawPaddles() {
-  context.fillStyle = "black";
-  context.fillRect(0, player1Y, paddleWidth, paddleHeight);
-  context.fillRect(
-    canvas.width - paddleWidth,
-    player2Y,
-    paddleWidth,
-    paddleHeight,
-  );
+class Paddle {
+  constructor() {
+    this.w = canvas.width * paddleConfig.width;
+    this.h = canvas.height * paddleConfig.height;
+    this.speed = canvas.height * paddleConfig.speed;
+  }
+
+  draw(x, y) {
+    context.fillStyle = "black";
+    context.fillRect(x, y, this.w, this.h);
+  }
+
+  reset() {
+    this.w = canvas.width * paddleConfig.width;
+    this.h = canvas.height * paddleConfig.height;
+    this.speed = canvas.height * paddleConfig.speed;
+  }
 }
 
-function drawBall() {
-  context.beginPath();
-  context.arc(ballX, ballY, ballSize, 0, Math.PI * 2);
-  context.fillStyle = "black";
-  context.fill();
-  context.closePath();
+class Ball {
+  constructor() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.size = Math.min(canvas.width, canvas.height) * ballConfig.size;
+    this.dx = Math.min(canvas.width, canvas.height) * ballConfig.dx;
+    this.dy = Math.min(canvas.width, canvas.height) * ballConfig.dy;
+  }
+
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    context.fillStyle = "black";
+    context.fill();
+    context.closePath();
+  }
+
+  reset() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+
+    this.size = Math.min(canvas.width, canvas.height) * ballConfig.size;
+    this.dx = Math.min(canvas.width, canvas.height) * ballConfig.dx;
+    this.dy = Math.min(canvas.width, canvas.height) * ballConfig.dy;
+  }
 }
+
+class Player {
+  constructor(xf, yf, scoref) {
+    this.xf = xf;
+    this.yf = yf;
+    this.scoref = scoref;
+    this.x = xf();
+    this.y = yf();
+    this.score = 0;
+    this.paddle = new Paddle();
+  }
+
+  draw() {
+    this.paddle.draw(this.x, this.y);
+    context.font = "30px Hack";
+    context.fillText(this.score, this.scoref(), 50);
+  }
+
+  reset() {
+    this.x = this.xf();
+    this.y = this.yf();
+    this.score = this.score;
+    this.paddle.reset();
+  }
+}
+
+let ball = new Ball();
+
+let p1 = new Player(
+  () => 0,
+  () => canvas.height / 2 - (canvas.height * paddleConfig.height) / 2,
+  () => 100,
+);
+
+let p2 = new Player(
+  () => canvas.width - canvas.width * paddleConfig.width,
+  () => canvas.height / 2 - (canvas.height * paddleConfig.height) / 2,
+  () => canvas.width - 100,
+);
+
+let PlayerEnum = {
+  ONE: "ONE",
+  TWO: "TWO",
+};
 
 function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawPaddles();
-  drawBall();
-
-  context.font = "30px Arial";
-  context.fillText(player1Score, 100, 50);
-  context.fillText(player2Score, canvas.width - 100, 50);
-}
-
-function update() {
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
-
-  handleBallCollisions();
-
-  if (ballX - ballSize <= 0) {
-    player2Score++;
-    reset();
-  }
-  if (ballX + ballSize >= canvas.width) {
-    player1Score++;
-    reset();
-  }
-}
-
-function handleBallCollisions() {
-  if (ballY - ballSize <= 0 || ballY + ballSize >= canvas.height) {
-    ballSpeedY = -ballSpeedY;
-  }
-
-  if (
-    ballX - ballSize <= paddleWidth &&
-    ballY >= player1Y &&
-    ballY <= player1Y + paddleHeight
-  ) {
-    ballSpeedX = -ballSpeedX;
-    let deltaY = ballY - (player1Y + paddleHeight / 2);
-    ballSpeedY = deltaY * 0.2;
-  }
-  if (
-    ballX + ballSize >= canvas.width - paddleWidth &&
-    ballY >= player2Y &&
-    ballY <= player2Y + paddleHeight
-  ) {
-    ballSpeedX = -ballSpeedX;
-    let deltaY = ballY - (player2Y + paddleHeight / 2);
-    ballSpeedY = deltaY * 0.2;
-  }
+  p1.draw();
+  p2.draw();
+  ball.draw();
 }
 
 function reset() {
-  ballX = canvas.width / 2;
-  ballY = canvas.height / 2;
+  ball.reset();
 }
 
-function gameLoop() {
-  update();
-  draw();
-  requestAnimationFrame(gameLoop);
+function handleBallCollisions() {
+  if (ball.y - ball.size <= 0 || ball.y + ball.size >= canvas.height) {
+    ball.dy = -ball.dy;
+  }
+
+  if (
+    ball.x - ball.size <= p1.x + p1.paddle.w &&
+    ball.y >= p1.y &&
+    ball.y <= p1.y + p1.paddle.h
+  ) {
+    ball.dx = -ball.dx;
+    let deltaY = ball.y - (p1.y + p1.paddle.h / 2);
+    ball.dy = deltaY * 0.2;
+  }
+  if (
+    ball.x + ball.size >= p2.x &&
+    ball.y >= p2.y &&
+    ball.y <= p2.y + p2.paddle.h
+  ) {
+    ball.dx = -ball.dx;
+    let deltaY = ball.y - (p2.y + p2.paddle.h / 2);
+    ball.dy = deltaY * 0.2;
+  }
 }
 
-gameLoop();
+function update() {
+  ball.update();
 
-window.addEventListener("keydown", function (event) {
-  switch (event.keyCode) {
-    case 38: // Up arrow key
-      if (player2Y > 0) {
-        player2Y -= paddleSpeed;
+  handleBallCollisions();
+
+  if (ball.x - ball.size <= 0) {
+    p2.score++;
+    reset();
+  }
+  if (ball.x + ball.size >= canvas.width) {
+    p1.score++;
+    reset();
+  }
+}
+
+function movePlayer(player, direction) {
+  switch (player) {
+    case PlayerEnum.ONE:
+      if (direction === Direction.UP) {
+        if (p1.y > 0) {
+          p1.y -= p1.paddle.speed;
+        }
+      } else if (direction === Direction.DOWN) {
+        if (p1.y < canvas.height - p1.paddle.h) {
+          p1.y += p1.paddle.speed;
+        }
       }
       break;
-    case 40: // Down arrow key
-      if (player2Y < canvas.height - paddleHeight) {
-        player2Y += paddleSpeed;
-      }
-      break;
-    case 87: // W key
-      if (player1Y > 0) {
-        player1Y -= paddleSpeed;
-      }
-      break;
-    case 83: // S key
-      if (player1Y < canvas.height - paddleHeight) {
-        player1Y += paddleSpeed;
+    case PlayerEnum.TWO:
+      if (direction === Direction.UP) {
+        if (p2.y > 0) {
+          p2.y -= p2.paddle.speed;
+        }
+      } else if (direction === Direction.DOWN) {
+        if (p2.y < canvas.height - p2.paddle.h) {
+          p2.y += p2.paddle.speed;
+        }
       }
       break;
   }
-});
+}
 
-window.addEventListener("resize", function () {
-  canvasWidth = window.innerWidth;
-  canvasHeight = window.innerHeight;
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
-  player1Y = canvas.height / 2 - paddleHeight / 2;
-  player2Y = canvas.height / 2 - paddleHeight / 2;
+window.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "w":
+      movePlayer(PlayerEnum.ONE, Direction.UP);
+      break;
+    case "s":
+      movePlayer(PlayerEnum.ONE, Direction.DOWN);
+      break;
+    case "ArrowUp":
+      movePlayer(PlayerEnum.TWO, Direction.UP);
+      break;
+    case "ArrowDown":
+      movePlayer(PlayerEnum.TWO, Direction.DOWN);
+      break;
+  }
 });
 
 canvas.addEventListener("touchstart", function (event) {
@@ -152,21 +226,13 @@ canvas.addEventListener("touchstart", function (event) {
   const halfScreenHeight = screenHeight / 2;
 
   if (touchX < halfScreenWidth && touchY < halfScreenHeight) {
-    if (player1Y > 0) {
-      player1Y -= paddleSpeed;
-    }
+    movePlayer(PlayerEnum.ONE, Direction.UP);
   } else if (touchX < halfScreenWidth && touchY >= halfScreenHeight) {
-    if (player1Y < canvas.height - paddleHeight) {
-      player1Y += paddleSpeed;
-    }
+    movePlayer(PlayerEnum.ONE, Direction.DOWN);
   } else if (touchX >= halfScreenWidth && touchY < halfScreenHeight) {
-    if (player2Y > 0) {
-      player2Y -= paddleSpeed;
-    }
+    movePlayer(PlayerEnum.TWO, Direction.UP);
   } else {
-    if (player2Y < canvas.height - paddleHeight) {
-      player2Y += paddleSpeed;
-    }
+    movePlayer(PlayerEnum.TWO, Direction.DOWN);
   }
 });
 
@@ -180,20 +246,31 @@ canvas.addEventListener("touchmove", function (event) {
   const halfScreenHeight = screenHeight / 2;
 
   if (touchX < halfScreenWidth && touchY < halfScreenHeight) {
-    if (player1Y > 0) {
-      player1Y -= paddleSpeed;
-    }
+    movePlayer(PlayerEnum.ONE, Direction.UP);
   } else if (touchX < halfScreenWidth && touchY >= halfScreenHeight) {
-    if (player1Y < canvas.height - paddleHeight) {
-      player1Y += paddleSpeed;
-    }
+    movePlayer(PlayerEnum.ONE, Direction.DOWN);
   } else if (touchX >= halfScreenWidth && touchY < halfScreenHeight) {
-    if (player2Y > 0) {
-      player2Y -= paddleSpeed;
-    }
+    movePlayer(PlayerEnum.TWO, Direction.UP);
   } else {
-    if (player2Y < canvas.height - paddleHeight) {
-      player2Y += paddleSpeed;
-    }
+    movePlayer(PlayerEnum.TWO, Direction.DOWN);
   }
 });
+
+window.addEventListener("resize", function () {
+  canvasWidth = window.innerWidth;
+  canvasHeight = window.innerHeight;
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+
+  p1.reset();
+  p2.reset();
+  ball.reset();
+});
+
+function gameLoop() {
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
